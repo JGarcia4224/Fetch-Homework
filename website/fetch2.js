dcca9ea93c11a9b4b3a90b4e0bd3fe222090be61
Jesse Garcia <Jesse.Garcia4224@gmail.com>
2018-03-13 13:58:35 -0700

    initial commit

---
+++ website/fetch2.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((data) => {
+                let output = '<h2>Users</h2>';
+                let myArray= [];
+				
+				data.forEach(function(user){
+                    myArray.push(user.username)
+
+					output += `
+                    <ul>
+                        <li> Name: ${user.name}</li>
+                    </ul>
+                    `;
+					myArray.sort(function(a, b){
+                  // ASC  -> a.length - b.length
+                  // DESC -> b.length - a.length
+                  return a.length - b.length;
+                });
+                    document.getElementById("userNames").innerHTML = myArray;
+                }); 
+            })
+        
+        
